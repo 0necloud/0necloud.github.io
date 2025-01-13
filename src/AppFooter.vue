f54d2515a5c2784e76dc6018876f334c55f2cb34
0necloud <hewyiyun@gmail.com>
2025-01-13 23:14:52 +0800

    Projects Update

--- src/AppFooter.vue
+++ src/AppFooter.vue
@@ -2,7 +2,7 @@
   <div class="layout-footer" style="color: white;">
     <img alt="Logo" src="res/images/logo-light.svg" height="30" class="mr-2" />
     Last Updated
-    <span class="font-medium ml-2 mr-2">08/01/25</span>
+    <span class="font-medium ml-2 mr-2">13/01/25</span>
   </div>
 </template>
 
